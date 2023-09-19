3e25096e7952db77530d8128cd986c4a74a8709c
davesalvador <101625754+davesalvador@users.noreply.github.com>
2023-09-19 10:47:43 +0800

    sidebaricons

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,8 +1,12 @@
+import DesktopSidebar from "./DekstopSidebar";
+
 async function Sidebar({ children }: { children: React.ReactNode }) {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <DesktopSidebar />
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  );
 }
 
 export default Sidebar;
-{
-  216;
-}
